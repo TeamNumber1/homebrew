f03afbd5a57b4ca9580dd6d628ecaeb0aba42b20
Dirk Gadsden <dirk@esherido.com>
2014-02-18 09:58:00 -0500

    Switch to block generator for command help

--- Library/Homebrew/help.rb
+++ Library/Homebrew/help.rb
@@ -1,15 +1,19 @@
 module Homebrew extend self
   def help_for cmd, &block
     cmd = cmd.to_sym
-    return cmd_help[cmd] if block.nil?
-    help_string = block.call
-    if help_string =~ /^\s+/
-      indent = $&
-      help_string = help_string.lines.map do |line|
-        line.sub /^#{indent}/, ''
-      end.join
+    if block.nil?
+      # Get the generator proc and call it to get a help string
+      gen = cmd_help[cmd]
+      help_string = gen.call
+      if help_string =~ /^\s+/
+        indent = $&
+        help_string = help_string.lines.map do |line|
+          line.sub /^#{indent}/, ''
+        end.join
+      end
+    else
+      cmd_help[cmd] = block
     end
-    cmd_help[cmd] = help_string
   end
   def print_help_for cmd
     puts help_for(cmd)
